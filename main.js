547ce23aa04ba9a4dc8dfe4f76318a22ac9e7c06
nemanjamalesija <malesija13@gmail.com>
2023-10-24 12:46:31 +0200

    Setup canvas and instantiate game class

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
 import './style.css';
-// import { setupCounter } from './counter.js';
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `;
+class Player {}
 
-// setupCounter(document.querySelector('#counter'));
+class Projectile {}
+
+class Enemy {}
+
+class Game {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+  }
+  render() {
+    console.log(this.height, this.width);
+  }
+}
+
+window.addEventListener('load', function () {
+  const canvas = document.getElementById('canvas1');
+  const ctx = canvas.getContext('2d');
+  canvas.width = 600;
+  canvas.height = 700;
+
+  const game = new Game(canvas);
+  console.log(game);
+});
